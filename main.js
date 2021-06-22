d895e8c37a2a7ad199fac5c730e89cc4c5b1a5cb
Islam Shaban <islamshaba998@gmail.com>
2021-06-22 10:18:54 +0300

    adding step2,3

--- main.js
+++ main.js
@@ -73,4 +73,187 @@ prevBtnForth.addEventListener( "click", function(e){
     progressCheck[current-2].classList.remove("active");
     current-=1;
 });
+function Active1() {
+    var checkBox = document.getElementById("feature1");
+    if (checkBox.checked === true){
+        document.getElementById("feature1-box").style.backgroundColor = "#05CAB610";
+        document.getElementById("feature1-box").style.color = "#05CAB6";
+        document.getElementById("feature1-box").style.border = "1.8px solid #05CAB6";
+    } else {
+        document.getElementById("feature1-box").style.backgroundColor = "#fff";
+        document.getElementById("feature1-box").style.color = "#0F0F0F";
+        document.getElementById("feature1-box").style.border = "1.8px solid #05CAB600";
+    }
+  }
+  function Active2() {
+    var checkBox = document.getElementById("feature2");
+    if (checkBox.checked === true){
+        document.getElementById("feature2-box").style.backgroundColor = "#05CAB610";
+        document.getElementById("feature2-box").style.color = "#05CAB6";
+        document.getElementById("feature2-box").style.border = "1.8px solid #05CAB6";
+    } else {
+        document.getElementById("feature2-box").style.backgroundColor = "#fff";
+        document.getElementById("feature2-box").style.color = "#0F0F0F";
+        document.getElementById("feature2-box").style.border = "1.8px solid #05CAB600";
+    }
+  }
+  function Active3() {
+    var checkBox = document.getElementById("feature3");
+    if (checkBox.checked === true){
+        document.getElementById("feature3-box").style.backgroundColor = "#05CAB610";
+        document.getElementById("feature3-box").style.color = "#05CAB6";
+        document.getElementById("feature3-box").style.border = "1.8px solid #05CAB6";
+    } else {
+        document.getElementById("feature3-box").style.backgroundColor = "#fff";
+        document.getElementById("feature3-box").style.color = "#0F0F0F";
+        document.getElementById("feature3-box").style.border = "1.8px solid #05CAB600";
+    }
+  }
+  function Active4() {
+    var checkBox = document.getElementById("feature4");
+    if (checkBox.checked === true){
+        document.getElementById("feature4-box").style.backgroundColor = "#05CAB610";
+        document.getElementById("feature4-box").style.color = "#05CAB6";
+        document.getElementById("feature4-box").style.border = "1.8px solid #05CAB6";
+    } else {
+        document.getElementById("feature4-box").style.backgroundColor = "#fff";
+        document.getElementById("feature4-box").style.color = "#0F0F0F";
+        document.getElementById("feature4-box").style.border = "1.8px solid #05CAB600";
+    }
+  }
+  function Active5() {
+    var checkBox = document.getElementById("feature5");
+    if (checkBox.checked === true){
+        document.getElementById("feature5-box").style.backgroundColor = "#05CAB610";
+        document.getElementById("feature5-box").style.color = "#05CAB6";
+        document.getElementById("feature5-box").style.border = "1.8px solid #05CAB6";
+    } else {
+        document.getElementById("feature5-box").style.backgroundColor = "#fff";
+        document.getElementById("feature5-box").style.color = "#0F0F0F";
+        document.getElementById("feature5-box").style.border = "1.8px solid #05CAB600";
+    }
+  }
+  function Active6() {
+    var checkBox = document.getElementById("feature6");
+    if (checkBox.checked === true){
+        document.getElementById("feature6-box").style.backgroundColor = "#05CAB610";
+        document.getElementById("feature6-box").style.color = "#05CAB6";
+        document.getElementById("feature6-box").style.border = "1.8px solid #05CAB6";
+    } else {
+        document.getElementById("feature6-box").style.backgroundColor = "#fff";
+        document.getElementById("feature6-box").style.color = "#0F0F0F";
+        document.getElementById("feature6-box").style.border = "1.8px solid #05CAB600";
+    }
+  }
+  function Active7() {
+    var checkBox = document.getElementById("feature7");
+    if (checkBox.checked === true){
+        document.getElementById("feature7-box").style.backgroundColor = "#05CAB610";
+        document.getElementById("feature7-box").style.color = "#05CAB6";
+        document.getElementById("feature7-box").style.border = "1.8px solid #05CAB6";
+    } else {
+        document.getElementById("feature7-box").style.backgroundColor = "#fff";
+        document.getElementById("feature7-box").style.color = "#0F0F0F";
+        document.getElementById("feature7-box").style.border = "1.8px solid #05CAB600";
+    }
+  }
+  function Active8() {
+    var checkBox = document.getElementById("feature8");
+    if (checkBox.checked === true){
+        document.getElementById("feature8-box").style.backgroundColor = "#05CAB610";
+        document.getElementById("feature8-box").style.color = "#05CAB6";
+        document.getElementById("feature8-box").style.border = "1.8px solid #05CAB6";
+    } else {
+        document.getElementById("feature8-box").style.backgroundColor = "#fff";
+        document.getElementById("feature8-box").style.color = "#0F0F0F";
+        document.getElementById("feature8-box").style.border = "1.8px solid #05CAB600";
+    }
+  }
 
+var checkBox = document.getElementById("feature1");
+if (checkBox.checked === true){
+    document.getElementById("feature1-box").style.backgroundColor = "#05CAB610";
+    document.getElementById("feature1-box").style.color = "#05CAB6";
+    document.getElementById("feature1-box").style.border = "1.8px solid #05CAB6";
+} else {
+    document.getElementById("feature1-box").style.backgroundColor = "#fff";
+    document.getElementById("feature1-box").style.color = "#0F0F0F";
+    document.getElementById("feature1-box").style.border = "1.8px solid #05CAB600";
+}
+
+var checkBox = document.getElementById("feature2");
+if (checkBox.checked === true){
+    document.getElementById("feature2-box").style.backgroundColor = "#05CAB610";
+    document.getElementById("feature2-box").style.color = "#05CAB6";
+    document.getElementById("feature2-box").style.border = "1.8px solid #05CAB6";
+} else {
+    document.getElementById("feature2-box").style.backgroundColor = "#fff";
+    document.getElementById("feature2-box").style.color = "#0F0F0F";
+    document.getElementById("feature2-box").style.border = "1.8px solid #05CAB600";
+}
+
+var checkBox = document.getElementById("feature3");
+if (checkBox.checked === true){
+    document.getElementById("feature3-box").style.backgroundColor = "#05CAB610";
+    document.getElementById("feature3-box").style.color = "#05CAB6";
+    document.getElementById("feature3-box").style.border = "1.8px solid #05CAB6";
+} else {
+    document.getElementById("feature3-box").style.backgroundColor = "#fff";
+    document.getElementById("feature3-box").style.color = "#0F0F0F";
+    document.getElementById("feature3-box").style.border = "1.8px solid #05CAB600";
+}
+
+var checkBox = document.getElementById("feature4");
+if (checkBox.checked === true){
+    document.getElementById("feature4-box").style.backgroundColor = "#05CAB610";
+    document.getElementById("feature4-box").style.color = "#05CAB6";
+    document.getElementById("feature4-box").style.border = "1.8px solid #05CAB6";
+} else {
+    document.getElementById("feature4-box").style.backgroundColor = "#fff";
+    document.getElementById("feature4-box").style.color = "#0F0F0F";
+    document.getElementById("feature4-box").style.border = "1.8px solid #05CAB600";
+}
+
+var checkBox = document.getElementById("feature5");
+if (checkBox.checked === true){
+    document.getElementById("feature5-box").style.backgroundColor = "#05CAB610";
+    document.getElementById("feature5-box").style.color = "#05CAB6";
+    document.getElementById("feature5-box").style.border = "1.8px solid #05CAB6";
+} else {
+    document.getElementById("feature5-box").style.backgroundColor = "#fff";
+    document.getElementById("feature5-box").style.color = "#0F0F0F";
+    document.getElementById("feature5-box").style.border = "1.8px solid #05CAB600";
+}
+
+var checkBox = document.getElementById("feature6");
+if (checkBox.checked === true){
+    document.getElementById("feature6-box").style.backgroundColor = "#05CAB610";
+    document.getElementById("feature6-box").style.color = "#05CAB6";
+    document.getElementById("feature6-box").style.border = "1.8px solid #05CAB6";
+} else {
+    document.getElementById("feature6-box").style.backgroundColor = "#fff";
+    document.getElementById("feature6-box").style.color = "#0F0F0F";
+    document.getElementById("feature6-box").style.border = "1.8px solid #05CAB600";
+}
+
+var checkBox = document.getElementById("feature7");
+if (checkBox.checked === true){
+    document.getElementById("feature7-box").style.backgroundColor = "#05CAB610";
+    document.getElementById("feature7-box").style.color = "#05CAB6";
+    document.getElementById("feature7-box").style.border = "1.8px solid #05CAB6";
+} else {
+    document.getElementById("feature7-box").style.backgroundColor = "#fff";
+    document.getElementById("feature7-box").style.color = "#0F0F0F";
+    document.getElementById("feature7-box").style.border = "1.8px solid #05CAB600";
+}
+
+var checkBox = document.getElementById("feature8");
+if (checkBox.checked === true){
+    document.getElementById("feature8-box").style.backgroundColor = "#05CAB610";
+    document.getElementById("feature8-box").style.color = "#05CAB6";
+    document.getElementById("feature8-box").style.border = "1.8px solid #05CAB6";
+} else {
+    document.getElementById("feature8-box").style.backgroundColor = "#fff";
+    document.getElementById("feature8-box").style.color = "#0F0F0F";
+    document.getElementById("feature8-box").style.border = "1.8px solid #05CAB600";
+}
